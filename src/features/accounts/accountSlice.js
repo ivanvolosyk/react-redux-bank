5ecf6dde0321b2ab7c0b69a9c99b439e12323901
Ivan Volosyk <volosyk.vanya@gmail.com>
2025-04-11 12:56:04 +0300

    Update the code using Redux Toolkit

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -1,11 +1,64 @@
-const initalStateAccount = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
   isLoading: false,
 };
 
-export default function accountReducer(state = initalStateAccount, action) {
+const accountSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    deposit(state, action) {
+      state.balance += action.payload;
+      state.isLoading = false;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestLoan: {
+      prepare(amount, purpose) {
+        return { payload: { amount, purpose } };
+      },
+      reducer(state, action) {
+        if (state.loan > 0) return;
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance = state.balance + action.payload.amount;
+      },
+    },
+    payLoan(state, action) {
+      state.balance -= state.loan;
+      state.loan = 0;
+      state.loanPurpose = "";
+    },
+    convertingCurrency(state) {
+      state.isLoading = true;
+    },
+  },
+});
+
+export const { withdraw, payLoan, requestLoan } = accountSlice.actions;
+
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+  return async function (dispatch, getState) {
+    // API call
+    dispatch({ type: "account/convertingCurrency" });
+    const res = await fetch(
+      `https://api.frankfurter.dev/v1/latest?base=${currency}&symbols=USD`
+    );
+    const data = await res.json();
+    const convertedAmount = Number((amount * data.rates["USD"]).toFixed(2));
+    console.log(convertedAmount);
+    dispatch({ type: "account/deposit", payload: convertedAmount });
+  };
+}
+
+export default accountSlice.reducer;
+/*export default function accountReducer(state = initalState, action) {
   switch (action.type) {
     case "account/deposit":
       return {
@@ -66,3 +119,4 @@ export function requestLoan(amount, purpose) {
 export function payLoan() {
   return { type: "account/payLoan" };
 }
+*/
--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,10 +1,42 @@
-const initalStateCustomer = {
+import { createSlice } from "@reduxjs/toolkit";
+import reducer from "../accounts/accountSlice";
+
+const initialState = {
   fullName: "",
   nationalID: "",
   createdAt: "",
 };
 
-export default function customerReducer(state = initalStateCustomer, action) {
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = action.payload.createdAt;
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
+
+export const { createCustomer, updateName } = customerSlice.actions;
+
+export default customerSlice.reducer;
+/*export default function customerReducer(state = initalState, action) {
   switch (action.type) {
     case "customer/createCustomer":
       return {
@@ -29,4 +61,4 @@ export function createCustomer(fullName, nationalID) {
 
 export function updateName(fullName) {
   return { type: "customer/updateName", payload: fullName };
-}
+}*/
